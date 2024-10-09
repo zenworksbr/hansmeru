2c73c996fdeb37a8f4b864258c2544a2330e3826
r47orr <razoroller529@gmail.com>
2024-10-08 23:33:31 -0300

    fix: corrigir inicialização da lang e corrigir funções princiais de geração de componentes

--- static/main.js
+++ static/main.js
@@ -1,16 +1,7 @@
-function getTranslationStrings(query) {
-        if (query != 'en' && query != 'pt') {
-                query = 'en'
-        }
-        const lang = document.createElement('script')
-        lang.src = `/static/${query}.js`
-        lang.type = 'text/javascript'
-        document.getElementsByTagName('head')[0].appendChild(lang)
-}
-
 function generatePFP(link) {
         const parent = document.getElementsByClassName('main_info')[0]
         pfp = document.createElement('img')
+        pfp.setAttribute('class', 'pfp')
         pfp.src = link
 
         parent.appendChild(pfp)
@@ -25,6 +16,7 @@ function generateUserInfo(name, title) {
         h4.innerText = title
         userInfo.appendChild(h3)
         userInfo.appendChild(h4)
+        console.log(userInfo)
 
         parent.appendChild(userInfo)
 }
@@ -35,19 +27,24 @@ function generateLinksTitle(about_links) {
         title.innerText = about_links
 }
 
-function generateLinkElement(data) {
+function generateLinkElement(title, data) {
 
         const linkItem = document.createElement('div')
+        linkItem.setAttribute('class', 'link_item') 
         const linkRedir = document.createElement('a')
         linkRedir.href = data.link
+        linkRedir.target = '_blank'
         linkItem.appendChild(linkRedir)
         const linkPic = document.createElement('div')
-        linkPic.class = 'image_container'
+        linkPic.setAttribute('class', 'image_container')
         const pic = document.createElement('img')
         pic.src = data.img_url
-        pic.class = 'link_img'
+        pic.setAttribute('class', 'link_img')
+        linkPic.appendChild(pic)
         const linkContent = document.createElement('div')
-        linkPic.class = 'link_container'
+        linkContent.setAttribute('class', 'link_container')
+        linkContent.innerText = title
+        
         linkRedir.appendChild(linkPic)
         linkRedir.appendChild(linkContent)
 
@@ -55,6 +52,8 @@ function generateLinkElement(data) {
 }
 
 function generateSpotifyFrame(link) {
+        const div = document.createElement('div')
+        div.setAttribute('class', 'link_item')
         const frame = document.createElement('iframe')
         frame.class = "link_item"
         frame.src = link
@@ -64,6 +63,7 @@ function generateSpotifyFrame(link) {
         frame.allowfullscreen="" 
         frame.allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
 
+        div.appendChild(frame)
         return frame
 }
 
@@ -72,7 +72,8 @@ function generateLinks(links) {
         for (const [title, data] of Object.entries(links)) {
                 if (title == 'spotify') {
                         parent.appendChild(generateSpotifyFrame(data))
+                        continue
                 }
-                parent.appendChild(generateLinkElement(data))
+                parent.appendChild(generateLinkElement(title, data))
         }
 }
